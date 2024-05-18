638265d69c78af2472c49ca37a102391e8b05b44
mrRavanCpp <161654833+mrRavanCpp@users.noreply.github.com>
2024-05-18 17:30:05 +0400

    N-Queens I, the second method.cpp

---
+++ Backtracking/Others/N-Queens I, the second method.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<string>> all;
+vector<string> board;
+
+bool isSafe(int row, int col, int n){    
+    for (int i = 0; i < n; ++i){
+        if (col - i >= 0 && board[row][col - i] == 'Q') return false;
+        if (row - i >= 0 && col - i >= 0 && board[row - i][col - i] == 'Q') return false;
+        if (row + i < n && col - i >= 0 && board[row + i][col - i] == 'Q') return false;
+    }
+    return true;
+}
+
+void backtrack(int col, int n){
+    if (col == n){
+        all.push_back(board); return;
+    }
+    
+    for (int row = 0; row < n; ++row){
+        if (isSafe(row, col, n)){
+            board[row][col] = 'Q';
+            backtrack(col + 1, n);
+            board[row][col] = '.';
+        }
+    }
+}
+
+int main(){
+    int n = 4;
+    string s(n, '.');
+    board.assign(n, s);
+    backtrack(0, n);
+    for (auto i: all){
+        for (string j: i){
+            cout << j << endl;
+        } cout << endl << endl;
+    }
+    
+    return 0;
+}

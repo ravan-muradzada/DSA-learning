482b0e70d2aad8eea2af9f9751f6affd6b42bfe8
mrRavanCpp <161654833+mrRavanCpp@users.noreply.github.com>
2024-06-06 01:36:42 +0400

    Rotate Array.cpp

---
+++ Array and its algorithms/Rotate Array.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<int>& nums, int k){
+    k = k % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+}
+
+int main(){
+    vector<int> nums = {1,2,3,4,5,6,7};
+    int k = 3;
+    rotate(nums, k);
+    
+    for (int i: nums) cout << i << " ";
+    
+    return 0;
+}

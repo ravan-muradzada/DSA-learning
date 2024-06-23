c21a76ee2548a89bbe7a7a41ec0052791fc404b7
mrRavanCpp <161654833+mrRavanCpp@users.noreply.github.com>
2024-06-24 01:36:58 +0400

    Coin Change.cpp

--- Dynamic Programming/Coin Change.cpp
+++ Dynamic Programming/Coin Change.cpp
@@ -27,23 +27,16 @@ int coinChangeMemoization(vector<int>& coins, int amount){
 
 int coinChangeTabulation(vector<int>& coins, int amount){
     int n = coins.size();
-    vector<vector<int>> dp(n, vector<int>(amount+1, 0));
-    
-    for (int i = 0; i <= amount; ++i){
-        dp[0][i] = (i % coins[0] == 0)? (i/coins[0]): 1e9;
-    }
-    
-    for (int i = 1; i < n; ++i){
-        for (int j = 0; j <= amount; ++j){
-            int notTake = dp[i-1][j];
-            int take = 1e9;
-            if (coins[i] <= j)
-                take = 1 + dp[i][j-coins[i]];
-            dp[i][j] = min(take, notTake);
+    vector<int> dp(amount+1, 1e9);
+    dp[0] = 0;
+    for (int i = 1; i <= amount; ++i){
+        for (int j: coins){
+            if (i - j >= 0){
+                dp[i] = min(dp[i], 1 + dp[i-j]);
+            }
         }
     }
-    int result = dp[n-1][amount];
-    return (result == 1e9)? -1: result;
+    return (dp[amount] == 1e9)? -1: dp[amount];
 }
 
 int main(){

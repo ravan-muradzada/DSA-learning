ba4ee0e17fc3c402921937aac82cb0b0cca5613e
mrRavanCpp <161654833+mrRavanCpp@users.noreply.github.com>
2024-06-23 22:45:04 +0400

    Coin Change.cpp

---
+++ Dynamic Programming/Coin Change.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int helperOfMemoization(vector<int>& coins, int amount, int indx, vector<vector<int>>& dp){
+    if (amount == 0) return 0;
+
+    if (indx == 0){
+        return (amount % coins[indx] == 0)? (amount/coins[indx]): 1e9;
+    }
+    
+    if (dp[indx][amount] != -1) return dp[indx][amount];
+    
+    int notTake = helperOfMemoization(coins, amount, indx - 1, dp);
+    int take = 1e9;
+    if (coins[indx] <= amount)
+        take = 1 + helperOfMemoization(coins, amount-coins[indx], indx, dp);
+    
+    return dp[indx][amount] = min(take, notTake);
+}
+
+int coinChangeMemoization(vector<int>& coins, int amount){
+    int n = coins.size();
+    vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+    int result = helperOfMemoization(coins, amount, n-1, dp);
+    return (result == 1e9)? -1: result;
+}
+
+int coinChangeTabulation(vector<int>& coins, int amount){
+    int n = coins.size();
+    vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+    
+    for (int i = 0; i <= amount; ++i){
+        dp[0][i] = (i % coins[0] == 0)? (i/coins[0]): 1e9;
+    }
+    
+    for (int i = 1; i < n; ++i){
+        for (int j = 0; j <= amount; ++j){
+            int notTake = dp[i-1][j];
+            int take = 1e9;
+            if (coins[i] <= j)
+                take = 1 + dp[i][j-coins[i]];
+            dp[i][j] = min(take, notTake);
+        }
+    }
+    int result = dp[n-1][amount];
+    return (result == 1e9)? -1: result;
+}
+
+int main(){
+    vector<int> coins = {3, 4, 1, 6};
+    int amount = 11;
+    
+    cout << coinChangeMemoization(coins, amount) << "\n\n";
+    cout << coinChangeTabulation(coins, amount) << "\n\n";
+    return 0;
+}

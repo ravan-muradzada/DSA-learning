e3af4640650be7cde9817e8b416d176776741117
mrRavanCpp <161654833+mrRavanCpp@users.noreply.github.com>
2024-07-10 23:22:55 +0400

    Spiral Matrix II.cpp

---
+++ Matrix/Spiral Matrix II.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> generateMatrix(int n){
+    vector<vector<int>> res(n, vector<int>(n));
+    int left = 0, right = n-1;
+    int up = 0, bottom = n-1;
+    int num = 1;
+    
+    while (left <= right && up <= bottom){
+        for (int i = left; i <= right; ++i){
+            res[up][i] = num++;
+        } up++;
+        
+        for (int i = up; i <= bottom; ++i){
+            res[i][right] = num++;
+        } right--;
+        
+        if (left > right || up > bottom) continue;
+        for (int i = right; i >= left; --i){
+            res[bottom][i] = num++;
+        } bottom--;
+        
+        if (left > right || up > bottom) continue;
+        for (int i = bottom; i >= up; --i){
+            res[i][left] = num++;
+        } left++;
+    }
+    
+    return res;
+}
+
+void printMatrix(vector<vector<int>>& matrix){
+    for (auto& i: matrix){
+        for (int j: i){
+            printf("%4d", j);
+        }
+        cout << endl;
+    }
+    cout << "\n\n\n";
+}
+
+int main(){
+    int n = 5;
+    
+    auto result = generateMatrix(n);
+    
+    for (auto& i: result){
+        for (int j: i) printf("%4d", j);
+        cout << endl;
+    }
+    
+    return 0;
+}
